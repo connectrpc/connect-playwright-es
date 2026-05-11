17684f8f550ae1d121422b7f49e62e0bb97c9ab5
Ajeet D'Souza <98ajeet@gmail.com>
2026-05-12 01:37:22 +0530

    [tmp] dry run
    Signed-off-by: Ajeet D'Souza <98ajeet@gmail.com>

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,4 +1,4 @@
-// Copyright 2024-2026 Buf Technologies, Inc.
+// Copyright 2021-2026 The Connect Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ npmPublish(version);
  */
 function npmPublish(version) {
   const tag = determinePublishTag(version);
-  execSync(`npm publish --tag ${tag} --workspaces`, {
+  execSync(`npm publish --dry-run --tag ${tag} --workspaces`, {
     stdio: "inherit",
   });
 }
